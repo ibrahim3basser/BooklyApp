985aa43c41162edeba0bbc672f3975d8f7dd0b71
ibrahim3basser <ibrahimelabassery2002@email.com>
2024-03-16 01:51:05 +0200

    add Custom App bar to home

---
+++ lib/Features/Splash/home/presentation/views/widgets/customAppBar.dart
@@ -0,0 +1,33 @@
+
+import 'package:booklyapp/Core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetData.logo,
+            height: 20,
+            ),
+          const Spacer(),
+          IconButton(
+            onPressed: (){
+      
+            }, 
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/Splash/home/presentation/views/widgets/homeViewBody.dart
@@ -0,0 +1,13 @@
+import 'package:booklyapp/Core/utils/assets.dart';
+import 'package:booklyapp/Features/Splash/home/presentation/views/widgets/customAppBar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomAppBar();
+  }
+}
